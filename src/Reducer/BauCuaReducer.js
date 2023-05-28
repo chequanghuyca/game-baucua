52d1240ebffdc226e07da636703f1d88c0619681
Huy Chè <chequanghuybtt@gmail.com>
2023-05-29 02:30:58 +0700

    big update

--- src/Reducer/BauCuaReducer.js
+++ src/Reducer/BauCuaReducer.js
@@ -1,6 +1,6 @@
 const initialState = {
   danhSachCuoc: [
-    { ma: 'nai', hinhAnh: './img/nai.png'},
+    { ma: 'nai', hinhAnh: './img/nai.png' },
     { ma: 'bau', hinhAnh: './img/bau.png' },
     { ma: 'ga', hinhAnh: './img/ga.png' },
     { ma: 'ca', hinhAnh: './img/ca.png' },
@@ -14,51 +14,193 @@ const initialState = {
   ],
   demKhacNhau: null,
   demCap: null,
-  bao: 1
+  bao: 1, 
+  remBef: null
 }
 
 const bauCuaReducer = (state = initialState) => {
   const dice = [...state.danhSachCuoc];
   let randomDice = [], resultDice = [];
-  let { demKhacNhau, demCap, bao } = state;
-  const listXucXac = {1: 'nai', 2: 'bau', 3: 'ga', 4: 'ca', 5: 'cua', 6: 'tom'};
-  const listBao = {1: 1, 2: 5, 3: 3, 4: 6, 5: 2, 6: 4 }; 
+  let { demKhacNhau, demCap, bao, remBef } = state;
+  const listXucXac = { 1: 'nai', 2: 'bau', 3: 'ga', 4: 'ca', 5: 'cua', 6: 'tom' };
+  const listBao = { 1: 1, 2: 5, 3: 3, 4: 6, 5: 2, 6: 4 }; 
 
   for (let i = 0; i < 3; i++) {
     const randomIndex = Math.floor(Math.random() * dice.length);
     randomDice.push(dice[randomIndex]);
   }
 
-  if (randomDice[0].ma !== randomDice[1].ma && randomDice[1].ma !== randomDice[2].ma && randomDice[0].ma !== randomDice[2].ma) {
+  if ((randomDice[0].ma !== randomDice[1].ma) && (randomDice[1].ma !== randomDice[2].ma) && (randomDice[0].ma !== randomDice[2].ma)) {
     if (demKhacNhau === 3 && demCap === 0) {
       demKhacNhau = 0;
       demCap = 0;
       console.log('Bão 3 con khác');
-      resultDice.push(...Array(3).fill({ma: listXucXac[listBao[bao]], hinhAnh: './img/' + listXucXac[listBao[bao]] + '.png' }));
+      resultDice.push(...Array(3).fill({ ma: listXucXac[listBao[bao]], hinhAnh: './img/' + listXucXac[listBao[bao]] + '.png' }));
       bao++;
-      if (bao === 7) bao = 1;
+      if (bao === 7) {
+        bao = 1;
+      } 
     } else {
       demCap = 0;
       demKhacNhau++;
-      resultDice = [...randomDice];
+      if (remBef === 'nai') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+        if (randomDice[0].ma !== 'cua' && randomDice[0].ma !== 'cua' && randomDice[2].ma !== 'cua') {
+          resultDice.push({ ma: 'cua', hinhAnh: './img/cua.png' });
+        } else {
+          resultDice.push({ ma: randomDice[2].ma, hinhAnh: './img/' + randomDice[2].ma + '.png' });
+        }
+      } else if (remBef === 'cua') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+        if (randomDice[0].ma !== 'ga' && randomDice[0].ma !== 'ga' && randomDice[2].ma !== 'ga') {
+          resultDice.push({ ma: 'ga', hinhAnh: './img/ga.png' });
+        } else {
+          resultDice.push({ ma: randomDice[2].ma, hinhAnh: './img/' + randomDice[2].ma + '.png' });
+        }
+      } else if (remBef === 'ga') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+        if (randomDice[0].ma !== 'tom' && randomDice[0].ma !== 'tom' && randomDice[2].ma !== 'tom') {
+          resultDice.push({ ma: 'tom', hinhAnh: './img/tom.png' });
+        } else {
+          resultDice.push({ ma: randomDice[2].ma, hinhAnh: './img/' + randomDice[2].ma + '.png' });
+        }
+      } else if (remBef === 'tom') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+        if (randomDice[0].ma !== 'bau' && randomDice[0].ma !== 'bau' && randomDice[2].ma !== 'bau') {
+          resultDice.push({ ma: 'bau', hinhAnh: './img/bau.png' });
+        } else {
+          resultDice.push({ ma: randomDice[2].ma, hinhAnh: './img/' + randomDice[2].ma + '.png' });
+        }
+      } else if (remBef === 'bau') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+        if (randomDice[0].ma !== 'ca' && randomDice[0].ma !== 'ca' && randomDice[2].ma !== 'ca') {
+          resultDice.push({ ma: 'ca', hinhAnh: './img/ca.png' });
+        } else {
+          resultDice.push({ ma: randomDice[2].ma, hinhAnh: './img/' + randomDice[2].ma + '.png' });
+        }
+      } else if (remBef === 'ca') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+        if (randomDice[0].ma !== 'nai' && randomDice[0].ma !== 'nai' && randomDice[2].ma !== 'nai') {
+          resultDice.push({ ma: 'nai', hinhAnh: './img/nai.png' });
+        } else {
+          resultDice.push({ ma: randomDice[2].ma, hinhAnh: './img/' + randomDice[2].ma + '.png' });
+        }
+      } else {
+        resultDice = [...randomDice];
+      }
     }
+    remBef = resultDice[0].ma;
+    
   } else if ((randomDice[0].ma === randomDice[1].ma && randomDice[0].ma !== randomDice[2].ma) || (randomDice[0].ma === randomDice[2].ma && randomDice[0].ma !== randomDice[1].ma) || (randomDice[1].ma === randomDice[2].ma && randomDice[1].ma !== randomDice[0].ma)) {
     if (demCap === 3 && demKhacNhau === 0) {
       demKhacNhau = 0;
       demCap = 0;
       console.log('Bão 2 con giống');
-      resultDice.push(...Array(3).fill({ma: listXucXac[listBao[bao]], hinhAnh: './img/' + listXucXac[listBao[bao]] + '.png' }));
+      resultDice.push(...Array(3).fill({ ma: listXucXac[listBao[bao]], hinhAnh: './img/' + listXucXac[listBao[bao]] + '.png' }));
       bao++;
-      if (bao === 7) bao = 1;
+      if (bao === 7) {
+        bao = 1;
+      }
     } else {
       demKhacNhau = 0;
       demCap++;
-      resultDice = [...randomDice];
+      if (remBef === 'nai') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        if (randomDice[0].ma !== 'cua') {
+          resultDice.push({ ma: 'cua', hinhAnh: './img/cua.png' });
+          resultDice.push({ ma: 'cua', hinhAnh: './img/cua.png' });
+        } else {
+          resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+          if (randomDice[1].ma !== 'cua') {
+            resultDice.push({ ma: 'cua', hinhAnh: './img/cua.png' });
+          } else {
+            resultDice.push({ ma: randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma, hinhAnh: './img/' + randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma + '.png' });
+          }
+        }
+      } else if (remBef === 'cua') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        if (randomDice[0].ma !== 'ga') {
+          resultDice.push({ ma: 'ga', hinhAnh: './img/ga.png' });
+          resultDice.push({ ma: 'ga', hinhAnh: './img/ga.png' });
+        } else {
+          resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+          if (randomDice[1].ma !== 'ga') {
+            resultDice.push({ ma: 'ga', hinhAnh: './img/ga.png' });
+          } else {
+            resultDice.push({ ma: randomDice[[1, 2, 4, 5, 6][Math.floor(Math.random() * 5)]].ma, hinhAnh: './img/' + randomDice[[1, 2, 4, 5, 6][Math.floor(Math.random() * 5)]].ma + '.png' });
+          }
+        }
+      } else if (remBef === 'ga') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        if (randomDice[0].ma !== 'tom') {
+          resultDice.push({ ma: 'tom', hinhAnh: './img/tom.png' });
+          resultDice.push({ ma: 'tom', hinhAnh: './img/tom.png' });
+        } else {
+          resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+          if (randomDice[1].ma !== 'tom') {
+            resultDice.push({ ma: 'tom', hinhAnh: './img/tom.png' });
+          } else {
+            resultDice.push({ ma: randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma, hinhAnh: './img/' + randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma + '.png' });
+          }
+        }
+      } else if (remBef === 'tom') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        if (randomDice[0].ma !== 'bau') {
+          resultDice.push({ ma: 'bau', hinhAnh: './img/bau.png' });
+          resultDice.push({ ma: 'bau', hinhAnh: './img/bau.png' });
+        } else {
+          resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+          if (randomDice[1].ma !== 'bau') {
+            resultDice.push({ ma: 'bau', hinhAnh: './img/bau.png' });
+          } else {
+            resultDice.push({ ma: randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma, hinhAnh: './img/' + randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma + '.png' });
+          }
+        }
+      } else if (remBef === 'bau') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        if (randomDice[0].ma !== 'ca') {
+          resultDice.push({ ma: 'ca', hinhAnh: './img/ca.png' });
+          resultDice.push({ ma: 'ca', hinhAnh: './img/ca.png' });
+        } else {
+          resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+          if (randomDice[1].ma !== 'ca') {
+            resultDice.push({ ma: 'ca', hinhAnh: './img/ca.png' });
+          } else {
+            resultDice.push({ ma: randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma, hinhAnh: './img/' + randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma + '.png' });
+          }
+        }
+      } else if (remBef === 'ca') {
+        resultDice.push({ ma: randomDice[0].ma, hinhAnh: './img/' + randomDice[0].ma + '.png' });
+        if (randomDice[0].ma !== 'nai') {
+          resultDice.push({ ma: 'nai', hinhAnh: './img/nai.png' });
+          resultDice.push({ ma: 'nai', hinhAnh: './img/nai.png' });
+        } else {
+          resultDice.push({ ma: randomDice[1].ma, hinhAnh: './img/' + randomDice[1].ma + '.png' });
+          if (randomDice[1].ma !== 'nai') {
+            resultDice.push({ ma: 'nai', hinhAnh: './img/nai.png' });
+          } else {
+            resultDice.push({ ma: randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma, hinhAnh: './img/' + randomDice[[1,2,3,4,6][Math.floor(Math.random() * 5)]].ma + '.png' });
+          }
+        }
+      } else {
+        resultDice = [...randomDice];
+      }
     }
+    remBef = resultDice[0].ma;
+
   } else {
-    resultDice.push(...Array(3).fill({ma: listXucXac[listBao[bao]], hinhAnh: './img/' + listXucXac[listBao[bao]] + '.png' }));
+    console.log('Bão ngẫu nhiên')
+    resultDice.push(...Array(3).fill({ ma: listXucXac[listBao[bao]], hinhAnh: './img/' + listXucXac[listBao[bao]] + '.png' }));
     bao++;
-    if (bao === 7) bao = 1;
+    if (bao === 7) {
+      bao = 1;
+    } 
     demKhacNhau = 0;
     demCap = 0;
   }
@@ -69,7 +211,8 @@ const bauCuaReducer = (state = initialState) => {
     danhSachCuoc: state.danhSachCuoc.map(dice => dice),
     demKhacNhau: demKhacNhau,
     demCap: demCap,
-    bao: bao
+    bao: bao, 
+    remBef: remBef
   };
 };
 
