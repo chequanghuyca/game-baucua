57fec366415f731c80f6fa56959d4161476f3157
Huy Chè <chequanghuybtt@gmail.com>
2023-12-14 03:40:07 +0700

    fixx

--- src/TomCuaCa/Baucua.jsx
+++ src/TomCuaCa/Baucua.jsx
@@ -5,11 +5,11 @@ import DanhSachXucXac from './DanhSachCuoc/DanhSachXucXac'
 export default function Baucua() {
   return (
     <div className='container' style={{ margin: 'auto', width: '100vw', height: '100vh', userSelect: 'none' }}>
-      <div style={{ height: '25%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{ height: '18%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <img src='./img/nen.png' alt='' style={{ width: '350px', marginTop: '15px' }} />
       </div>
 
-      <div style={{ height: '75%', width: '100%' }}>
+      <div style={{ height: '82%', width: '100%' }}>
         <DanhSachQuanCuoc />
 
         <DanhSachXucXac />
